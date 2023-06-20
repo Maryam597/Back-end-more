bd1b5146fbf8a50c37a1106acd4a27154e57ee75
Maryam597 <maryam.dri@hotmail.fr>
2023-06-20 15:03:22 +0200

    P2

--- includes/form2.php
+++ includes/form2.php
@@ -1,43 +1,131 @@
 
-<?php include './includes/form.html'; ?>
+
+<form action="./index.php" method="post" class="row" enctype="multipart/form-data">
 
 
-<form action="#" method="post">
+<fieldset class="col-md-7">
 
-<div class="card col-md-4 mx-auto my-1">
+<div class="form-floating mb-3 mt-3">
+        <input  type="text" name="firstname" placeholder="Prénom" />
 
-<label for="connaissances">Connaissances</label>
 
-<div class="languages">
+    </div>
 
-<input type="checkbox" name="knowledge" value="HTML">HTML</input>
-<input type="checkbox" name="knowledge" value="CSS">CSS</input>
-<input type="checkbox" name="knowledge" value="JavaScript">JavaScript</input>
-<input type="checkbox" name="knowledge" value="PHP">PHP</input>
-<input type="checkbox" name="knowledge" value="MySQL">MySQL</input>
-<input type="checkbox" name="knowledge" value="Bootstrap">Bootstrap</input>
-<input type="checkbox" name="knowledge" value="Symfony">Symfony</input>
-<input type="checkbox" name="knowledge" value="React">React</input>
+    <br>
+
+    <div class="form-floating mb-3">
+        <input type="text" name="lastname" placeholder="Nom" />
+    </div>
+
+
+    <br>
+
+    <div class="form-group">
+        <label for="age">Age (18 à 70 ans)</label>
+        <input type="number" name="age" min="18" max="60" value="Renseignez votre âge"
+        placeholder="Renseignez votre age" class="form-control" id="age"/>
+    </div>
+
+  
+
+
+    <br>
+
+    <div class="form-group input-group">
+        <div class="input-group-prepend">
+        <label for="height" class="input-group-text">Taille (1,26m à 3m)</label>
+    </div>
+
+        <input type="number" class="form-control" name="height"  min="1.26" max="3.00" step="0.01" placeholder="Renseignez votre taille" />
+    <div class="input-group-append">
+        <span class="input-group-text">m</span>
+      </div>
+</div>
+
+
+    <br>
+
+    <div class="d-flex mb-3" >
+        <input class="form-check-label" type="radio" name="gender" value="Homme">Homme
+        <input class="form-check-label" type="radio" name="gender" value="Femme">Femme
+    </div>
+
+    <br>
+
+   
+
+</fieldset>
+
+<fieldset class="col-md-3">
+
+<!-- <div class="card col-md-4 mx-auto my-1"> -->
+
+<legend>Connaissances</legend>
+
+<div class="languages" type="submit">
+
+<div>
+<input type="checkbox" name="HTML" value="HTML"></input>
+<label for="">HTML</label>
+
+</div>
+
+<div>
+<input type="checkbox" name="CSS" value="CSS"></input>
+<label for="">CSS</label>
+</div>
+
+<div>
+<input type="checkbox" name="JavaScript" value="JavaScript"></input>
+<label for="">JavaScript</label>
+</div>
+
+<div>
+    <input type="checkbox" name="PHP" value="PHP"></input>
+    <label for="">PHP</label>
+    </div>
+
+<div>
+<input type="checkbox" name="MySQL" value="MySQL"></input>
+<label for="">MySQL</label>
+</div>
+
+
+<div>
+    <input type="checkbox" name="Bootstrap" value="Bootstrap"></input>
+<label for="">Bootstrap</label>
+    </div>
+
+<div>
+<input type="checkbox" name="Symfony" value="Symfony"></input>
+<label for="">Symfony</label>
+</div>
+
+<div>
+
+<input type="checkbox" name="React" value="React"></input>
+<label for="">React</label>
+</div>
 
 </div>
 
 <div class="colors">
 
 <label for="favcolor">Couleur préférée</label>
-<input type="color" id="favouritecolor" name="favouritecolor" value="#000000">
+<input type="color" id="favouritecolor" name="favouritecolour" value="#000000">
 
 </div>
 
 <div class="date of birth">
-<label for="birthday">Date de naissance</label>
-<input type="date" id="birthday" name="birthday">
+<label for="date of birth">Date de naissance</label>
+<input type="date" id="date of birth" name="date of birth">
 
 </div>
 
 
 
-</div>
 
+</fieldset>
 
 
 
@@ -48,12 +136,14 @@
 <div action="index.php" method="POST" enctype="multipart/form-data">
         <label for="file">Parcourir</label>
         <input type="file" name="file">
-        <!-- <button type="submit">Enregistrer</button> -->
 
 </div>
 
 </div>
+<div class="d-flex justify-content-end col-md-7">
+        <input class="btn btn-primary"  type="submit" value="Enregistrer" name="form" />
+        
 
-
+    </div>
 
 </form>
--- index.php
+++ index.php
@@ -21,20 +21,18 @@ session_start();
       <a href="index.php"> <button name="homebutton" type="button" class="btn btn-dark">Home</button></a>
 
 
-      <?php  if (!isset($_GET['add'])) {
+      <?php if (!isset($_GET['add'])) {
 
 
-      echo '<a href="index.php?add"> <button type="button" class="btn btn-dark">Ajouter des données</button></a>';
-
-    }
-
-
-    if (!isset($_GET['addmore'])) {
+        echo '<a href="index.php?add"> <button type="button" class="btn btn-dark">Ajouter des données</button></a>';
+      }
 
 
-      echo '<a href="index.php?addmore"> <button type="button" class="btn btn-dark">Ajouter plus de données</button></a>';
-   
-    }
+      if (!isset($_GET['addmore'])) {
+
+
+        echo '<a href="index.php?addmore"> <button type="button" class="btn btn-dark">Ajouter plus de données</button></a>';
+      }
 
 
       if (isset($_SESSION['table'])) {
@@ -42,7 +40,7 @@ session_start();
       }
       ?>
 
-      
+
 
     </nav>
 
@@ -56,16 +54,11 @@ session_start();
 
       if (isset($_GET['add'])) {
         include_once './includes/form.html';
-      } 
+      }
 
       if (isset($_GET['addmore'])) {
         include_once './includes/form2.php';
-      } 
-      
-      
-      
-      
-      elseif (isset($_POST['form'])) {
+      } elseif (isset($_POST['form']) || (isset($_POST['form2']))) {
 
 
         $table = [
@@ -73,13 +66,88 @@ session_start();
           'lastname' => $_POST['lastname'],
           'age' => $_POST['age'],
           'height' => $_POST['height'],
-          'gender' => $_POST['gender']
+          'gender' => $_POST['gender'],
         ];
+
+
+        if (isset($_POST['favouritecolour'])) {  // vérification de l'existence de ''
+          $table['favouritecolour'] = $_POST['favouritecolour']; // ajouter la valeur dans $table
+        }
+
+        if (isset($_POST['date of birth'])) {
+          $table['date of birth'] = $_POST['date of birth'];
+        }
+
+        if (isset($_POST['HTML'])) {
+          $table['HTML'] = $_POST['HTML'];
+        }
+
+        if (isset($_POST['CSS'])) {
+          $table['CSS'] = $_POST['CSS'];
+        }
+
+        if (isset($_POST['JavaScript'])) {
+          $table['JavaScript'] = $_POST['JavaScript'];
+        }
+
+        if (isset($_POST['PHP'])) {
+          $table['PHP'] = $_POST['PHP'];
+        }
+
+        if (isset($_POST['MySQL'])) {
+          $table['MySQL'] = $_POST['MySQL'];
+        }
+
+        if (isset($_POST['Bootstrap'])) {
+          $table['Bootstrap'] = $_POST['Bootstrap'];
+        }
+        if (isset($_POST['Symfony'])) {
+          $table['Symfony'] = $_POST['Symfony'];
+        }
+        if (isset($_POST['React'])) {
+          $table['React'] = $_POST['React']; // $table clé    post valeur
+        }
+
+
+        if (isset($_FILES)) {
+          var_dump($_FILES);
+          $tmpName = $_FILES['file']['tmp_name'];
+          $type = $_FILES['file']['type'];
+          $name = $_FILES['file']['name'];
+          $size = $_FILES['file']['size'];
+          $error = $_FILES['file']['error'];
+
+
+
+          $tabExtension = explode('.', $name); // séparer les caractères des points
+          $extension = strtolower(end($tabExtension)); // permet de mettre en min pour éviter les erreurs de lecture 
+
+          $extensions = ['jpg', 'png'];
+
+          $maxSize = 200000;
+
+          if (in_array($extension, $extensions) && $size <= $maxSize) { // vérification taille et extension
+            move_uploaded_file($tmpName, './uploaded/' . $name); // stocker image dans un dossier
+          } else {
+            echo "Mauvaise extension ou taille trop grande";
+          }
+          $table['img'] = array( //création d'un tableau dans un tableau
+            'tmp_name' => $tmpName, // clé
+            'type' => $type,
+            'name' => $name,
+            'size' => $size,
+            'error' => $error,
+          );
+
+
+
+        }
+
+
+
+
         $_SESSION['table'] = $table;
         echo '<h3>Données sauvegardées</h3>';
-
-
-
       } elseif (isset($_SESSION['table'])) {
 
 
@@ -91,11 +159,10 @@ session_start();
         }
 
         echo '</pre>';
-
       }
 
 
-  
+
 
 
 
@@ -104,61 +171,68 @@ session_start();
 
         echo '<h2> Concaténation </h2>';
 
-        echo ( '<p>' . 'M.' . ' ' . ucfirst($_SESSION['table']['firstname']) . ' ' . strtoupper($_SESSION['table']['lastname']));
+        echo ('<p>' . 'M.' . ' ' . ucfirst($_SESSION['table']['firstname']) . ' ' . strtoupper($_SESSION['table']['lastname']));
 
 
-        echo '<p>' . ' J\'ai' .' ' . $_SESSION['table']['age'] .' ' . 'ans et je mesure' .' ' . $_SESSION['table']['height'] .' ' . 'mètre.';
+        echo '<p>' . ' J\'ai' . ' ' . $_SESSION['table']['age'] . ' ' . 'ans et je mesure' . ' ' . $_SESSION['table']['height'] . ' ' . 'mètre.';
 
 
 
         // str_replace(',', '.', ['table']['height']);
 
 
-        }
-         
-     
-        if (isset($_GET['loop'])) {
-
-          echo '<h2>Boucle</h2>';
-					echo '<h3> = = => Lecture du tableau à l\'aide d\'une boucle foreach</h3>';
-					
-          foreach ($_SESSION['table'] as $key => $value) {
-
-            echo '<h5>' .$key . ':' . $value . '</h5>';
-
-          }
-        }
-
-
-          if (isset($_GET['function'])) {
-            $table = $_SESSION['table'];
-            echo '<h2>Fonction</h2>';
-						echo '<h3>===> Lecture du tableau à l\'aide d\'une boucle foreach</h3>';
-
-            function readTable($table) {
-              $n = 0;
-              foreach ($_SESSION['table'] as $key => $value ) {
-
-                echo 'à la ligne n°' . $n++ . ' ' . 'correspond la clé' . ' ' . '"' . $key . '"' . ' ' . 'et contient' . ' ' . '"' . $value . '"' . "<br>";
-              }
-            }
-
-            readTable($table);
-
-
-          }
-        
-
-          
-      elseif (isset($_GET['del'])) {
-
-        session_destroy();
       }
 
 
-        elseif (isset($_POST['form2']))
+      if (isset($_GET['loop'])) {
+
+        echo '<h2>Boucle</h2>';
+        echo '<h3> = = => Lecture du tableau à l\'aide d\'une boucle foreach</h3>';
+
+        $n = 0;
+
+        foreach ($_SESSION['table'] as $key => $value) {
+
+
+          echo 'à la ligne n°' . $n++ . ' ' . 'correspond la clé' . ' ' . '"' . $key . '"' . ' ' . 'et contient' . ' ' . '"' . $value . '"' . "<br>";
+        }
+      }
+
+
+      if (isset($_GET['function'])) {
+        $table = $_SESSION['table'];
+        echo '<h2>Fonction</h2>';
+        echo '<h3>===> Lecture du tableau à l\'aide d\'une boucle foreach</h3>';
+
+        function readTable($table)
+        {
+          $n = 0;
+          foreach ($_SESSION['table'] as $key => $value) {
+
+            echo 'à la ligne n°' . $n++ . ' ' . 'correspond la clé' . ' ' . '"' . $key . '"' . ' ' . 'et contient' . ' ' . '"' . $value . '"' . "<br>";
+          }
+        }
+
+        readTable($table);
+      } elseif (isset($_GET['del'])) {
+
+        session_destroy();
+      } elseif (isset($_POST['form2']))
+
+        //         if(isset($_POST['submit'])) //to run PHP script on submit
+        // {   
+        // if(!empty($_POST['check_list'])){
+        // // Loop to store and display values of individual checked checkbox.
+        // foreach($_POST['check_list'] as $selected){
+        // echo $selected."</br>";
+        // }
+        // }
+        // }
 
       ?>
+
+
+
 
 
 
